bdd4dfec6e7cb9c2f998bd773f2179fcbffc9807
TheoOrigin <theoorigin@example.com>
2024-07-03 16:12:33 +0200

    edit part 1 for bonus

--- groupe-01-partie-1/src/fonctions.rs
+++ groupe-01-partie-1/src/fonctions.rs
@@ -1,41 +1,69 @@
 use std::net::{IpAddr, ToSocketAddrs};
+use ipnetwork::IpNetwork;
 use ping::ping;
 
 pub fn host_is_up(address: &str) -> bool {
-    //passage de l'adresse en ip
+    //conversion de l'adresse en IpAddr
     let ip_addr: IpAddr = match address.parse() {
-        //si c'est un ip valide, on l'utilise
         Ok(ip) => ip,
-        //si elle est pas valide, on essaie de la passer en nom de domaine
         Err(_) => {
-            //utiliser to_socket_addrs pour trouver l'adresse
             match (address, 0).to_socket_addrs() {
-                //obtenir la première adresse trouvé
                 Ok(mut addrs) => match addrs.next() {
-                    //on utilise l'adresse ip trouvé
                     Some(addr) => addr.ip(),
-                    //si aucune adresse est trouvée return false
                     None => {
-                        println!("connexion a l'adresse : {} impossible", address);
+                        println!("Connexion à l'adresse {} impossible", address);
                         return false;
                     },
                 },
                 Err(_) => {
-                    println!("error {}", address);
+                    println!("Erreur {}", address);
                     return false;
                 },
             }
         }
     };
 
+    //vérification du ping
     match ping(ip_addr, None, None, None, None, None) {
-        //si le ping réussi return true
-        Ok(_) => { 
-            true
-        },
-        Err(err) => {
-            println!("ping echoué : {}, error: {}", ip_addr, err);
+        Ok(_) => true,
+        Err(_err) => {
+            println!("Ping échoué pour {}", ip_addr);
             false
         },
+    }
+}
+
+pub fn check_ip_range(ip_range: &str) {
+    //passage de l'adresse CIDR en IpNetwork
+    let ip_network: IpNetwork = match ip_range.parse() {
+        Ok(network) => network,
+        Err(_) => {
+            println!("Adresse CIDR invalide : {}", ip_range);
+            return;
+        }
+    };
+
+    //vérification du type d'adresse
+    match ip_network {
+        IpNetwork::V4(v4_network) => {
+            let ips = v4_network.iter();
+            for ip in ips {
+                let octets = ip.octets();
+                //ignore les ip .0 et .255
+                if octets[3] == 0 || octets[3] == 255 {
+                    continue;
+                }
+                
+                let ip_addr = IpAddr::V4(ip);
+                if host_is_up(&ip_addr.to_string()) {
+                    println!("Adresse {} est en ligne", ip_addr);
+                } else {
+                    println!("Adresse {} est hors ligne", ip_addr);
+                }
+            }
+        },
+        IpNetwork::V6(_) => {
+            println!("IPv6 n'est pas supporté pour la plage d'adresses.");
+        }
     }
 }
