99dea6c8bbb622f1c691f9a114de3f8773a2ae6a
Jiullian Ferreira <jiullian77470@gmail.com>
2024-07-03 16:51:22 +0200

    feat - improve part 4 with port scan

--- groupe-01-partie-4/src/main.rs
+++ groupe-01-partie-4/src/main.rs
@@ -5,20 +5,19 @@ use std::net::IpAddr;
 use target::Target;
 
 fn main() {
-    //création d'une nouvelle cible avec une adresse IP
-    let ip_address: IpAddr = "10.33.24.1".parse().expect("Adresse IP invalide");
+    // Création d'une nouvelle cible avec une adresse IP
+    let ip_address: IpAddr = "10.33.1.24".parse().expect("Adresse IP invalide");
     let mut target = Target::new(ip_address);
 
-    //affichage de l'adresse IP
+    // Affichage de l'adresse IP
     println!("Adresse IP : {}", target.ip_addr());
 
-    //affichage de l'état initial de disponibilité de la cible
-    println!("cible en ligne ? : {}\n", target.is_up());
+    // Affichage de l'état initial de disponibilité de la cible
+    println!("Cible en ligne ? : {}\n", target.is_up());
 
-
-    //ajout de quelque ports
+    // Ajout de quelques ports et vérification s'ils sont ouverts
     target.add_open_port(80);
     target.add_open_port(443);
     target.add_open_port(22);
-    println!("ports 'ouverts' : {:?}", target.open_ports());
+    println!("Ports 'ouverts' : {:?}", target.open_ports());
 }
