6946335a7383d9624c2c8c17c61097cfa492a480
Jiullian Ferreira <jiullian77470@gmail.com>
2024-07-03 17:00:37 +0200

    feat - add comentary

--- groupe-01-partie-2/src/scanner.rs
+++ groupe-01-partie-2/src/scanner.rs
@@ -2,6 +2,9 @@
 use std::net::{TcpStream, SocketAddr};
 use std::time::Duration;
 
+/// Fonction pour scanner un port sur une adresse IP donnée
+/// Cette fonction tente de se connecter au port spécifié sur l'adresse IP donnée.
+/// Si la connexion réussit, le port est considéré comme ouvert, sinon il est considéré comme fermé.
 pub fn scan_port(ip: &str, port: u16) -> bool {
     //Créer une chaîne de caractères contenant l'adresse IP et le port
     let address = format!("{}:{}", ip, port);
